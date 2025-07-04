db14c4a3260288bdd99a0f690617b7dee47d1d05
bmspd <my_spid@mail.ru>
2025-07-04 21:53:28 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://soul-development.github.io',
+  base: 'julia-coverage-check',
+  integrations: [react()]
+});
